d33e625e108abd933d3287d61e11c815724ca304
liushuochen <2524453728@qq.com>
2019-09-07 23:51:07 +0800

    v5.0 系统平台更新

--- platform.py
+++ platform.py
@@ -47,17 +47,30 @@ def login():
     with open(pwd_conf_path, "r") as pwd_file:
         data = json.load(pwd_file)
     pwd_file.close()
+
     system_username = data["username"]
+    has_login = data["login"]
+    if has_login:
+        print("Login system failed.")
+        print("FusionStudent system has been logged in.")
+        return False
+
     if username == system_username:
         password = getpass.getpass("# password: ")
         if password == data["password"]:
             print("Login system success.")
+            data["login"] = True
+            with open(pwd_conf_path, "w") as pwd_file:
+                json.dump(data, pwd_file, indent=4)
+            pwd_file.close()
             return True
         else:
             print("Login system failed.")
+            print("Please input correct password.")
             return False
     else:
         print("Login system failed.")
+        print("Please input correct username.")
         return False
 
 
@@ -65,5 +78,3 @@ def login():
 if __name__ == "__main__":
     if login():
         main()
-    else:
-        print("Password wrong. Please input correct password.")
